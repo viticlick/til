c11a9b7132cd17c29e7647c5627543f696be8e5b
Víctor López <viticlick@users.noreply.github.com>
2020-02-15 11:53:46 +0100

    Create bash_scritps_template.sh

---
+++ linux/bash_scritps_template.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Short description of the script
+
+set -e errexit  # the script ends if a command fails
+set -e pipefail # the script ends if a command fails in a pipeline
+set -e nounset  # the script ends if it uses and undeclared variable
+# set -o xtrace # if you want to debug
